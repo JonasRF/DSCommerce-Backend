09782274b6ba89673dea02be1559814ece4e4fae
JonasRF <jonasribeiro41@yahoo.com.br>
2024-04-29 21:21:29 -0300

    Upload endPoint

--- backend/src/main/java/com/devsuperior/DSCommerce/DsCommerceApplication.java
+++ backend/src/main/java/com/devsuperior/DSCommerce/DsCommerceApplication.java
@@ -1,13 +1,10 @@
 package com.devsuperior.DSCommerce;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DsCommerceApplication {
+public class DsCommerceApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsCommerceApplication.class, args);
